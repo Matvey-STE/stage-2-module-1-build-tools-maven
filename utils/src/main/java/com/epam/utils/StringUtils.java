2caabba72ae70dcde32223f7a74e6dd5348fb151
matvey <matveyvs1987@gmail.com>
2023-09-09 01:01:22 +0200

    First attempt

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,11 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        try {
+            int number = Integer.parseInt(str);
+            return number > 0;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
